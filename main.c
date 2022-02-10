04b75755b93324b3d4c148b96487092f496c0e63
Wartk Gtesk <78173997+Wartk-Gtesk@users.noreply.github.com>
2022-02-10 17:41:22 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
+#ifndef WARTKBITS_H
+#define WARTKBITS_H
+
 #include <stdio.h>
 #include <stdint.h>
 
-void printBinary(uint64_t binary, uint8_t bits){
+void printBinary(uint64_t binary, uint8_t bits, uint8_t breakapart){
    
    uint64_t maxbit = 0;
    
@@ -15,18 +18,25 @@ void printBinary(uint64_t binary, uint8_t bits){
        maxbit = 0x8000000000000000;
    }
    
-   for(int i = 0; i < bits; i++){
-       printf("%c", (binary & maxbit) ? '1' : '0');
+     for(int i = 0; i < bits; i++){
+         
+     if(breakapart == 4){
+         if(i == breakapart || i == 8 || i == 12 || i == 16 || i == 20 || i == 24 || i == 28 || i == 32 || i == 36 || i == 40 || i == 44 || i == 48 || i == 52 || i == 56 || i == 60 || i == 64){
+           printf(" ");
+         }
+     }
+     
+     if(breakapart == 8){
+         if(i == breakapart || i == 16 || i == 24 || i == 32 || i == 40 || i == 48 || i == 56 || i == 64){
+           printf(" ");
+         }
+     }
+         
+        printf("%c", (binary & maxbit) ? '1' :        '0');
+       
        maxbit /= 2;
    }
    
 }
 
-int main(void){
-    
-    uint8_t x = 5;
-    
-    printBinary(x, 8);
-    
-    return 0;
-}
+#endif
